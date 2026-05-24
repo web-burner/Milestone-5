ad2d1a6299c54246039624ab98b92575cb363457
ExploitX <exploitx@ExploitXs-MacBook-Air.local>
2026-05-24 17:17:04 +0800

    transaction features applied

--- Module 26: Let’s Create Payooo Mobile Bank (Part 1)/home.js
+++ Module 26: Let’s Create Payooo Mobile Bank (Part 1)/home.js
@@ -30,25 +30,7 @@ function removeInput(id, id2, id3) {
 function showForms(id) {
   return getElement(id).classList.remove("hidden");
 }
-function addMoney() {
-  let balance = parseInt(getElement("primary-balance").innerText);
-  const bank = getElement("bank-name").value;
-  const accountNumber = parseInt(getElement("account-number").value);
-  const pinNumber = parseInt(getElement("pin-number").value);
-  const addAmount = parseInt(getElement("add-money-amount").value);
 
-  if (bank !== "" && accountNumber === validAccount && pinNumber === validPin) {
-    if (addAmount > 0 && typeof addAmount === "number") {
-      const newBalance = balance + addAmount;
-      setBalance(newBalance);
-      return;
-    } else {
-      alert("Invalid Amount");
-    }
-  } else {
-    alert("Invalid Credentials");
-  }
-}
 function activeBtn(id) {
   getElements(".features-btn").forEach((btn) => {
     btn.classList.remove("border-blue-400");
@@ -63,6 +45,54 @@ function hideForms() {
   forms.forEach((form) => form.classList.add("hidden"));
 }
 
+function getData (){
+  console.log()
+}
+const data = [];
+function storeTransaction(transact) {
+  data.push(transact);
+  console.log(data);
+  getElement("transaction-container").innerHTML = "";
+  data.map((e) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <div class="flex justify-between items-center border-2 p-2 my-2 rounded-2xl border-gray-300">
+                    <div>
+                        <p class="font-bold">${e.method}</p>
+                        <p>${e.time}</p>
+                    </div>
+                    <div>
+                        <p class="font-bold ${e.amount.includes("+") ? "text-green-600" : "text-red-600"}">${e.amount}</p>
+                    </div>
+                </div>`;
+    getElement("transaction-container").append(card);
+  });
+}
+function addMoney() {
+  let balance = parseInt(getElement("primary-balance").innerText);
+  const bank = getElement("bank-name").value;
+  const accountNumber = parseInt(getElement("account-number").value);
+  const pinNumber = parseInt(getElement("pin-number").value);
+  const addAmount = parseInt(getElement("add-money-amount").value);
+
+  if (bank !== "" && accountNumber === validAccount && pinNumber === validPin) {
+    if (addAmount > 0 && typeof addAmount === "number") {
+      const newBalance = balance + addAmount;
+      setBalance(newBalance);
+      storeTransaction({
+        method: bank,
+        accountNumber: accountNumber,
+        amount: "+" + addAmount,
+        time: new Date().toLocaleString(),
+      });
+      return;
+    } else {
+      alert("Invalid Amount");
+    }
+  } else {
+    alert("Invalid Credentials");
+  }
+}
 getElement("add-money").addEventListener("click", (e) => {
   hideForms();
   showForms("add-money-form");
@@ -98,6 +128,12 @@ function cashOut(account, pin, amount) {
     if (cashOutAmount > 0 && typeof cashOutAmount === "number") {
       const newBalance = balance - cashOutAmount;
       setBalance(newBalance);
+      storeTransaction({
+        method: bank,
+        accountNumber: accountNumber,
+        amount: "-" + cashOutAmount,
+        time: new Date().toLocaleString(),
+      });
       return;
     } else {
       alert("Invalid Amount");
@@ -162,4 +198,10 @@ getElement("bill-btn").addEventListener("click", (e) => {
   cashOut("bill-user-number", "bill-pin-number", "bill-amount");
   alert(`${billMethod} paid successfully`);
   removeInput("bill-user-number", "bill-pin-number", "bill-amount");
+});
+
+getElement("transaction-form-btn").addEventListener("click", (e) => {
+  hideForms();
+  showForms("transaction-form");
+  activeBtn("transaction-form-btn");
 });
