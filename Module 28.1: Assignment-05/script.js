7b1203f763629f95b3d86a1ed230094202b8fdbe
ExploitX <exploitx@ExploitXs-MacBook-Air.local>
2026-05-27 02:44:11 +0800

    assignment done

---
+++ Module 28.1: Assignment-05/script.js
@@ -0,0 +1,75 @@
+// console.log("hello");
+function getElement(id) {
+  return document.getElementById(id);
+}
+function setInnerText(id, value) {
+  return (document.getElementById(id).innerText = value);
+}
+const data = [];
+getElement("service-container").addEventListener("click", (e) => {
+  let balance = parseInt(getElement("balance").innerText);
+  let heartCount = parseInt(getElement("heart-count").innerText);
+  if (e.target.className.includes("call-btn")) {
+    const hotLine =
+      e.target.parentNode.parentNode.childNodes[5].childNodes[1].innerText;
+    const serviceName =
+      e.target.parentNode.parentNode.childNodes[3].childNodes[3].innerText;
+
+    if (balance >= 20) {
+      const newBalance = balance - 20;
+      setInnerText("balance", newBalance);
+      data.unshift({
+        name: serviceName,
+        number: hotLine,
+        date: new Date().toLocaleDateString(),
+        time: new Date().toLocaleTimeString(),
+      });
+      showHistory();
+      alert(`📞 Calling ${serviceName} ${hotLine}... `);
+    } else {
+      alert("You Need $20 To Call A Service");
+    }
+  }
+  if (e.target.className.includes("heart-btn")) {
+    e.target.classList.add("text-red-500");
+    const newCount = (heartCount += 1);
+    setInnerText("heart-count", newCount);
+  }
+  if (e.target.className.includes("copy-btn")) {
+    let copyCount = parseInt(getElement('copy-count').innerText)
+    const copyText =
+      e.target.parentNode.parentNode.childNodes[5].childNodes[1].innerText;
+      navigator.clipboard.writeText(copyText)
+      .then(()=>{
+        const newCopy = copyCount+=1
+        setInnerText('copy-count',newCopy)
+        alert(`${copyText} copied successfully!`)
+      }).catch((err)=>{
+        console.error('Failed to copy text',err)
+      })
+  }
+});
+
+function showHistory() {
+  getElement("cart-container").innerHTML = "";
+  data.map((e) => {
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="flex justify-between items-center bg-gray-100 rounded-lg p-3 mb-2">
+                    <div>
+                        <p class="text-lg font-semibold">${e.name}</p>
+                        <p class="text-sm">${e.number}</p>
+                    </div>
+                    <div>
+                        <p>${e.time}</p>
+                        <p>${e.date}</p>
+                    </div>
+                </div>`;
+    getElement("cart-container").appendChild(div);
+  });
+}
+
+getElement("clear-btn").addEventListener("click", (e) => {
+  data.length = 0;
+  showHistory();
+  alert("Cleared All Call History");
+});
